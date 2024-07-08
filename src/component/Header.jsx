18e8f68c76d5b0dab2ec8759fa937450d923aca2
ashutosh-dgksocial <ashutoshdgksocial@gmail.com>
2024-07-08 18:21:09 +0530

    done each exapt home

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,93 +5,158 @@ import { IoPersonOutline } from "react-icons/io5";
 import { CiHeart } from "react-icons/ci";
 import { BsCart2 } from "react-icons/bs";
 import Group from '../assets/Group.webp';
+import cart from '../assets/cart.webp';
+
 
 const Header = () => {
-  const [show, setShow] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const [showCart, setShowCart] = useState(false);
+  const [showBasket, setShowBasket] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleCloseMenu = () => setShowMenu(false);
+  const handleShowMenu = () => setShowMenu(true);
+
+  const handleCloseBasket = () => setShowBasket(false);
+  const handleShowBasket = () => setShowBasket(true);
+
+  const handleCloseCart = () => setShowCart(false);
+  const handleShowCart = () => setShowCart(true);
 
   return (
     <>
-      <Navbar expand="lg" className="shadow" style={{ zIndex: '1', position: 'sticky', top: '0', background: 'white' }}>
-        <Container fluid className="px-5 d-flex justify-content-between align-items-center">
-          <Link className="navbar-brand" to="#">
-            <img src={Group} alt="Teen Troops" height="50" />
-          </Link>
-          <Navbar.Toggle aria-controls="offcanvasNavbar" onClick={handleShow} />
-          <Navbar.Collapse id="navbarNav" className="justify-content-center">
-            <Nav className='gap-5'>
-              <Nav.Item>
-                <Link className="nav-link fw-semibold text-black" aria-current="page" to="/home">HOME</Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Link className="nav-link fw-semibold text-black" to="/">COMIC CON</Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Link className="nav-link fw-semibold text-black" to="/man">MEN'S</Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Link className="nav-link fw-semibold text-black" to="/women">WOMEN'S</Link>
-              </Nav.Item>
-            </Nav>
-          </Navbar.Collapse>
-          <Nav className="d-lg-flex gap-3">
-            <Nav.Item style={{ borderRight: '1px solid black', borderColor: '#D3D3D3' }}>
-              <Link className="nav-link" to="#">
-                <IoPersonOutline size={24} />
+      <div style={{ zIndex: '1', position: 'sticky', top: '0', }}>
+        <div>
+          <Navbar expand="lg" className="shadow" style={{ zIndex: '1', position: 'sticky', top: '0', background: 'white' }}>
+            <Container fluid className="px-5 d-flex justify-content-between align-items-center">
+              <Link className="navbar-brand" to="#">
+                <img src={Group} alt="Teen Troops" height="50" />
               </Link>
-            </Nav.Item>
-            <Nav.Item style={{ borderRight: '1px solid black', borderColor: '#D3D3D3' }}>
-              <Link className="nav-link" to="/wishlist">
-                <CiHeart size={24} />
-              </Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link className="nav-link" to="#">
-                <BsCart2 size={24} />
-              </Link>
-            </Nav.Item>
-          </Nav>
-        </Container>
-      </Navbar>
+              <Navbar.Toggle aria-controls="offcanvasNavbar" onClick={handleShowMenu} />
+              <Navbar.Collapse id="navbarNav" className="justify-content-center">
+                <Nav className='gap-5'>
+                  <Nav.Item>
+                    <Link className="nav-link fw-semibold text-black" aria-current="page" to="/home">HOME</Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Link className="nav-link fw-semibold text-black" to="/">COMIC CON</Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Link className="nav-link fw-semibold text-black" to="/man">MEN'S</Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Link className="nav-link fw-semibold text-black" to="/women">WOMEN'S</Link>
+                  </Nav.Item>
+                </Nav>
+              </Navbar.Collapse>
+              <Nav className="d-lg-flex gap-3"> {/* Show on large screens */}
+                <Nav.Item style={{ borderRight: '1px solid black', borderColor: '#D3D3D3' }}>
+                  <Link className="nav-link" to="#">
+                    {/* First icon people */}
+                    <IoPersonOutline size={24} onClick={handleShowCart} />
+                  </Link>
+                </Nav.Item>
+                <Nav.Item style={{ borderRight: '1px solid black', borderColor: '#D3D3D3' }}>
+                  <Link className="nav-link" to="/wishlist">
+                    <CiHeart size={24} />
+                  </Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Link className="nav-link" to="#">
+                    <BsCart2 size={24} onClick={handleShowBasket} />
+                  </Link>
+                </Nav.Item>
+              </Nav>
+            </Container>
+          </Navbar>
 
-      <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Menu</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <Nav className="flex-column">
-            <Nav.Item>
-              <Link className="nav-link fw-semibold text-black" aria-current="page" to="/home">HOME</Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link className="nav-link fw-semibold text-black" to="/">COMIC CON</Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link className="nav-link fw-semibold text-black" to="/man">MEN'S</Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link className="nav-link fw-semibold text-black" to="/women">WOMEN'S</Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link className="nav-link" to="#">
-                <IoPersonOutline />
-              </Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link className="nav-link" to="/wishlist">
-                <CiHeart />
-              </Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link className="nav-link" to="#">
-                <BsCart2 />
-              </Link>
-            </Nav.Item>
-          </Nav>
-        </Offcanvas.Body>
-      </Offcanvas>
+          <Offcanvas show={showMenu} onHide={handleCloseMenu}>
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title>Menu</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="flex-column">
+                <Nav.Item>
+                  <Link className="nav-link fw-semibold text-black" aria-current="page" to="/home">HOME</Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Link className="nav-link fw-semibold text-black" to="/">COMIC CON</Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Link className="nav-link fw-semibold text-black" to="/man">MEN'S</Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Link className="nav-link fw-semibold text-black" to="/women">WOMEN'S</Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Link className="nav-link" to="#">
+                    <IoPersonOutline />
+                  </Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Link className="nav-link" to="/wishlist">
+                    <CiHeart />
+                  </Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Link className="nav-link" to="#">
+                    <BsCart2 />
+                  </Link>
+                </Nav.Item>
+              </Nav>
+            </Offcanvas.Body>
+          </Offcanvas>
+        </div>
+
+        {/* people offcanvas */}
+        <div>
+          <Offcanvas show={showCart} onHide={handleCloseCart} placement="end">
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title>Cart</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <div className="col-md-12 px-3">
+                <div className="login-form">
+                  <h2 className='text-center h3 text-secondary'>Login</h2>
+                  <form>
+                    <div className="form-group">
+                      <input type="text" className="form-control rounded" id="username" placeholder="Username" />
+                    </div>
+                    <div className="form-group">
+                      <input type="password" className="form-control rounded" id="password" placeholder="Password" />
+                    </div>
+                    <div className="form-group py-3">
+                      <a href="#" className="forgot-password text-secondary">Forgot Password</a>
+                    </div>
+                    <button type="submit" className="btn-hov btn border border-1 border-dark w-100 rounded">Login</button>
+                    <button type="button" className="btn btn-dark w-100 mt-2 rounded">Continue With Google</button>
+                  </form>
+                  <div className="register-link text-start">
+                    <p>Don't have an account? <a href="#">Register</a></p>
+                    <p>Already registered? <span>Email not verified</span></p>
+                  </div>
+                </div>
+              </div>
+            </Offcanvas.Body>
+          </Offcanvas>
+        </div>
+
+        {/* cart offcanvas */}
+        <div>
+          <Offcanvas show={showBasket} onHide={handleCloseBasket} placement="end">
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title>Cart</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <div className='w-100 col-12 d-flex flex-column justify-content-center align-items-center'>
+                <img src={cart} alt="cart" width='200' height='250' style={{ objectFit: 'cover' }} />
+                <h3>Cart is empty</h3>
+                <p>Look like you have no items in your shopping cart.</p>
+              </div>
+
+            </Offcanvas.Body>
+          </Offcanvas>
+        </div>
+      </div>
     </>
   );
 };
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
-
+import Carousel from 'react-bootstrap/Carousel';
+import slider1 from "../assets/slider (1).png"
+import slider2 from "../assets/slider (2).png"
+import slider3 from "../assets/slider (3).png"
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <div>
+        <Carousel>
+          <Carousel.Item>
+            <img src={slider1} alt="slider1" style={{ width: "100%", objectFit: "cover" }} />
+
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={slider2} alt="slider2" style={{ width: "100%", objectFit: "cover" }} />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={slider3} alt="slider3" style={{ width: "100%", objectFit: "cover" }} />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </>
   )
 }
 
