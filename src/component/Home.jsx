ef9978908639cd4d71ddfdbbcd0937546c5dc271
ashutosh-dgksocial <ashutoshdgksocial@gmail.com>
2024-07-08 18:56:59 +0530

    second last commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,6 +2,10 @@ import Carousel from 'react-bootstrap/Carousel';
 import slider1 from "../assets/slider (1).png"
 import slider2 from "../assets/slider (2).png"
 import slider3 from "../assets/slider (3).png"
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import player1 from "../assets/players (1).png"
+import player2 from "../assets/players (2).png"
+import player3 from "../assets/players (3).png"
 function Home() {
   return (
     <>
@@ -37,12 +41,45 @@ function Home() {
           </Carousel>
         </div>
 
-        <div className='container'>
-          <h2>Categories</h2>
+        {/* line  */}
+        <div className='container py-3'>
+          <h4>Categories</h4>
           <div className='d-flex align-items-center' style={{ background: '#ef6566', width: '8rem', height: '5px', position: 'relative' }}>
             <div style={{ position: 'absolute', left: '8rem', right: '', borderBottom: '2px solid grey', width: 'calc(100% + 50rem)' }}></div>
           </div>
+          {/* player  */}
+
+          <div className='d-flex justify-content-center gap-5 py-5'>
+            <div className="mycard ">
+              <Card className="my-card-shadow " style={{ width: '19rem', border: 'none', }}>
+                <Card.Img variant="top" src={player3} style={{ borderRadius: '10px', height: '18rem', objectFit: 'cover', }} />
+              </Card>
+            </div>
+            <div className="mycard ">
+              <Card className="my-card-shadow" style={{ width: '19rem', border: 'none', }}>
+                <Card.Img variant="top" src={player2} style={{ borderRadius: '10px', height: '18rem', objectFit: 'cover', }} />
+              </Card>
+            </div>
+            <div className="mycard ">
+              <Card className="my-card-shadow" style={{ width: '19rem', border: 'none', }}>
+                <Card.Img variant="top" src={player1} style={{ borderRadius: '10px', height: '18rem', objectFit: 'cover', }} />
+              </Card>
+            </div>
+          </div>
         </div>
+        {/* box */}
+        <Container className="bg-light py-5 border border-2 border-secondary">
+          <Row className="justify-content-center">
+            <Col md={8}>
+              <h1 className="text-center mb-4">Unbeatable Offers Await!</h1>
+              <p className="text-center">Summer week's special discount</p>
+              <h3 className="text-center">Get Exciting Offers! Use Code:</h3>
+              <button variant="primary" className=" btn btn-light border border-secondary d-block mx-auto" size="lg">SUMMER50</button>
+            </Col>
+          </Row>
+        </Container>
+
+        {/* last div */}
       </div>
     </>
   )
